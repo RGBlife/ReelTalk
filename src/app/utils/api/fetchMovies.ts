f56943ba21174332127c4f208c002186598ee96d
Bret <bret_gomes811@hotmail.co.uk>
2023-11-08 12:30:28 +0000

    adjust fetchMovies to be more readable

--- src/app/utils/api/fetchMovies.ts
+++ src/app/utils/api/fetchMovies.ts
@@ -3,5 +3,6 @@ import { PaginationSchema } from "../paginationFilter";
 
 export const fetchMovies = async ({ limit, page }: PaginationSchema) => {
   const response = await fetch(`/api/movies?limit=${limit}&page=${page}`);
-  return (await response.json()) as Movie[];
+  const movies: Movie[] = await response.json();
+  return movies;
 };
