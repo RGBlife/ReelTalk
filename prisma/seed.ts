910986af87afdcb018a7160fd414e35330c3f288
Bret <bret_gomes811@hotmail.co.uk>
2023-11-08 18:16:57 +0000

    add placeholder image url to users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -657,6 +657,8 @@ async function main() {
         email: "oceanic@gmail.com",
         password:
           "$2b$12$Tux5AC8Sns/V/SHY8I.Tfe6y4vJX55JOX.LMKV9Q/cn.hTAUV6vXW",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "Admin",
         favourite_movie: "The Abyss",
         is_watch_list_public: true,
@@ -667,6 +669,8 @@ async function main() {
         email: "coralreef@gmail.com",
         password:
           "$2b$12$B31pkyvMQMOgH40eBIxKvuJNQ8Rsiq7q6LVRPBiK7n8SvfHEQ4Cqm",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Coraline",
         is_watch_list_public: true,
@@ -677,6 +681,8 @@ async function main() {
         email: "starfish@gmail.com",
         password:
           "$2b$12$X3cB.NSTLkju3UVZDjbLjephwXtac2bWO6TEwW7riaC6/OL6DeR22",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "A Star is Born",
         is_watch_list_public: true,
@@ -687,6 +693,8 @@ async function main() {
         email: "marlinblue@gmail.com",
         password:
           "$2b$12$ZWBIYkK/P6lz9LxQjfBxPuhO4ST/Pkpbrw21jo7HtBaNa/NyomKDG",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Big Fish",
         is_watch_list_public: true,
@@ -697,6 +705,8 @@ async function main() {
         email: "seaurchin@gmail.com",
         password:
           "$2b$12$OFhN8YCyprYU8MnOq0piZeS8UtTuDSDbvMkvd2.K/VbjVST11gode",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Ocean's Eleven",
         is_watch_list_public: true,
@@ -707,6 +717,8 @@ async function main() {
         email: "dolphinwave@gmail.com",
         password:
           "$2b$12$xlaq2/maGdDlR.H3Rfl/G.Oo9si1m7Iqpij7UL.N.ux5/Tqt9Fmqm",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Flipper",
         is_watch_list_public: true,
@@ -717,6 +729,8 @@ async function main() {
         email: "sharkfin@gmail.com",
         password:
           "$2b$12$FWYopWWcoAPYDnwTkmhntOTWFdK8.q9bJUNR4m.o/pTIT7NEREdzW",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Jaws",
         is_watch_list_public: true,
@@ -727,6 +741,8 @@ async function main() {
         email: "seahorse@gmail.com",
         password:
           "$2b$12$KtClvniJJPF.mL/T2DMPQuMaAYKSAspT9sBuJQKI5nEVBWpCsFm2W",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Seabiscuit",
         is_watch_list_public: true,
@@ -737,6 +753,8 @@ async function main() {
         email: "clownfish@gmail.com",
         password:
           "$2b$12$3gZ/8oFus8548m3ZYdmkTusJf.SYAAPH5rwYPqv9V/kwVVjQlALXu'",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Finding Nemo",
         is_watch_list_public: true,
@@ -747,6 +765,8 @@ async function main() {
         email: "mantaray@gmail.com",
         password:
           "$2b$12$9wvS.JJmNj3ZjXt5n5Ft3OWe1NnWsr5tI/qGA9QIfBJwL8Hdr2XJq",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "The Deep",
         is_watch_list_public: true,
@@ -757,6 +777,8 @@ async function main() {
         email: "squidink@gmail.com",
         password:
           "$2b$12$fMrLPeZ.WhJwzNm3101JWez7JlreRhSu/UGPXq/jAQ8yOJs700D9C",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "20,000 Leagues Under the Sea",
         is_watch_list_public: true,
@@ -767,6 +789,8 @@ async function main() {
         email: "anemone@gmail.com",
         password:
           "$2b$12$HzvFebdo1J/ngmsT1Cng2e5JrMizW9vlMtu8Y5/Y02zz25pqjigOa'",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Aquaman",
         is_watch_list_public: true,
@@ -777,6 +801,8 @@ async function main() {
         email: "pelicanpoint@gmail.com",
         password:
           "$2b$12$PxVeayUbRxNZQmErwUzOe.ygVUk9OQ0QGDclYe7tcoNo2XUTNx8Qm",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "The Birds",
         is_watch_list_public: true,
@@ -787,6 +813,8 @@ async function main() {
         email: "turtlebay@gmail.com",
         password:
           "$2b$12$ynkbOOt/0t19vkoNpFJtHeRBKCs3VbRKoudf.uoUP2xBPvbtyVOLC",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Tortoise and the Hare",
         is_watch_list_public: true,
@@ -797,6 +825,8 @@ async function main() {
         email: "jellyfish@gmail.com",
         password:
           "$2b$12$i8Xq1Gu7ct3oa9locMqq1.OjO2hxUOYmKZWg763U0Rba3fcMmRtX2",
+        avatar_url:
+          "https://api.iconify.design/clarity/avatar-solid.svg?width=360&height=360",
         role: "User",
         favourite_movie: "Finding Nemo",
         is_watch_list_public: true,
