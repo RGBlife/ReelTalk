2fe83e491aa47ce472589d8b8a22dd817a2f80b6
Bret <bret_gomes811@hotmail.co.uk>
2023-11-08 12:28:09 +0000

    api fetch variable changed from result to response

--- src/app/utils/api/fetchMovies.ts
+++ src/app/utils/api/fetchMovies.ts
@@ -2,6 +2,6 @@ import { Movie } from "@prisma/client";
 import { PaginationSchema } from "../paginationFilter";
 
 export const fetchMovies = async ({ limit, page }: PaginationSchema) => {
-  const result = await fetch(`/api/movies?limit=${limit}&page=${page}`);
-  return (await result.json()) as Movie[];
+  const response = await fetch(`/api/movies?limit=${limit}&page=${page}`);
+  return (await response.json()) as Movie[];
 };
