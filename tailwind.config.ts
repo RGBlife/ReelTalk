21279348d313df2e90a06a7c062c9cfc8baa0458
Bret <bret_gomes811@hotmail.co.uk>
2023-11-10 16:39:07 +0000

    add brand colour

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        primary: "#B996F7",
+        neutral: "#F8F9F9",
+      },
     },
   },
   plugins: [],
