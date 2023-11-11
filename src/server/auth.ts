11a1c16ffc38588020204324d54b925edf8331a9
Bret <bret_gomes811@hotmail.co.uk>
2023-11-11 10:01:12 +0000

    add signIn back in

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -41,7 +41,7 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   pages: {
-    // signIn: "/auth/login",
+    signIn: "/auth/login",
   },
   session: {
     strategy: "jwt",
