08408845c215cd7c37aa75ef748343d6f6c70ceb
Bret <bret_gomes811@hotmail.co.uk>
2023-11-12 21:52:53 +0000

    2nd attempt to add colour theme to daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,18 +15,24 @@ export default {
     },
   },
   plugins: [require("daisyui")],
+  // daisyUI config (optional - here are the default values)
   daisyui: {
-    base: false,
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color
     themes: [
       {
-        daisyuiCustomTheme: {
-          primary: "#B996F7",
+        mytheme: {
+          "primary": "#B996F7",
           "primary-focus": "#9673e8",
-          "primary-content": "#ffffff",
+          "primary-content": "#F8F9F9",
+          "base-100": "#F8F9F9",
         },
       },
-      "light",
-      "dark",
     ],
   },
 } satisfies Config;
