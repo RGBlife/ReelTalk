9c6d2d8c994dc92dd28ad5740f5ef12d5b3f4ca8
Bret <bret_gomes811@hotmail.co.uk>
2023-11-13 19:14:36 +0000

    make sidenav sticky

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -3,27 +3,25 @@ import Link from "next/link";
 
 export default function SideNav() {
   return (
-    <nav className="top-0 flex flex-col justify-between px-2 py-4">
-      <ul className="flex h-[90vh] flex-col items-start gap-5 whitespace-nowrap">
-        <li className="rounded-md border-2 border-solid border-black">
+    <nav className="sticky left-0 top-0 z-50 flex flex-col justify-between rounded-md border-r-2 border-solid border-black px-2 py-4">
+      <ul className="flex h-[95vh] flex-col items-start gap-5 whitespace-nowrap">
+        <li>
           <Link href={`/`}>Home</Link>
         </li>
-        <li className="rounded-md border-2 border-solid border-black">
+        <li>
           Profile
         </li>
-        <li className="rounded-md border-2 border-solid border-black">
+        <li>
           <Link href={`/movies`}>All Movies</Link>
         </li>
-        <li className="rounded-md border-2 border-solid border-black">
+        <li>
           <Link href={`/movies/recommendations`}>Recommendations</Link>
         </li>
-        <li className="rounded-md border-2 border-solid border-black">
-          <Link href={`/chat-room`}>Chat Room</Link>
-        </li>
+
+        <button className="mt-auto">
+          Log In/Out
+        </button>
       </ul>
-      <button className="rounded-md border-2 border-solid border-black">
-        Log In/Out
-      </button>
     </nav>
   );
 }
