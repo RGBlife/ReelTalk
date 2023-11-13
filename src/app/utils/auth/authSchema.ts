ec098cc3a7a247bb2d658769144834b9a238b3df
Hayley <hayley.mcc@gmail.com>
2023-11-13 17:28:53 +0000

    Created register form and styled it. Created registration backend with zod validation. Users will be redirected if already logged in. Added package needed to validate react hook form with zod

---
+++ src/app/utils/auth/authSchema.ts
@@ -0,0 +1,36 @@
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email().trim().toLowerCase(),
+  password: z.string(),
+});
+
+export const RegisterSchema = z
+  .object({
+    email: z.string().email().trim().toLowerCase(),
+    password: z
+      .string()
+      .min(6, { message: "Password must be a minimum of 6 characters" })
+      .max(24),
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Password must be a minimum of 6 characters" })
+      .max(24),
+    username: z.string().trim().toLowerCase().min(2, {
+      message: "Username must be a minimum of 2 or more characters",
+    }),
+    favouriteMovie: z.string().trim().min(2, {
+      message: "Favourite movie must be a minimum of 2 or more characters",
+    }),
+    avatarURL: z
+      .string()
+      .trim()
+      .toLowerCase()
+      .url()
+      .optional()
+      .or(z.literal("")),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
