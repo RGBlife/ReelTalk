128000350d9790c3bf96f58288c676768673aa58
chiara2201 <71043804+chiara2201@users.noreply.github.com>
2023-11-14 20:58:48 +0000

    adds authentication to review like button

--- src/app/(app)/movies/[id]/components/Review.tsx
+++ src/app/(app)/movies/[id]/components/Review.tsx
@@ -5,98 +5,26 @@ import { ReviewDeleteButton } from "./ReviewDeleteButton";
 import { genRelativeDateStr } from "~/utils/date-formatters";
 import Image from "next/image";
 import type { ReviewSectionReviews } from "./ReviewSection";
-import { ReviewLikeButtonOptimistic } from "./ReviewLikeButtonOptimistic";
+import { ReviewLikeButton } from "./ReviewLikeButton";
 import Link from "next/link";
-<<<<<<< Updated upstream
-import { StarIcon } from "@heroicons/react/20/solid";
-=======
-import { getServerAuthSession } from "~/server/auth";
 import { useSession } from "next-auth/react";
->>>>>>> Stashed changes
+import { createClassName } from "~/utils/string-formatters";
+import { StarIcon } from "@heroicons/react/20/solid";
 
 type Props = {
   review: ReviewSectionReviews;
   index: number;
 };
 
-<<<<<<< Updated upstream
-function createClassName(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
-//removed key from props
-export const Review = ({ review, index }: Props) => {
-  return (
-    <>
-      <div
-        key={review.id}
-        className="flex w-full space-x-4 text-sm text-gray-500"
-      >
-        <div
-          className={createClassName(
-            index === 0 ? "py-4" : "border-t border-gray-400 py-10",
-            "w-full",
-          )}
-        >
-          <div className="flex justify-between ">
-            <div>
-              <Link href={`/profiles/${review.author.username}`}>
-                <h3 className="font-medium text-gray-400">
-                  {review.author.username}
-                </h3>
-              </Link>
-              <p className="text-gray-600">
-                {genRelativeDateStr(review.created_at)}
-              </p>
-            </div>
-
-            <div>
-              <div className="flex items-center">
-                {[0, 1, 2, 3, 4].map((rating) => (
-                  <StarIcon
-                    key={rating}
-                    className={createClassName(
-                      review.rating > rating
-                        ? "text-yellow-400"
-                        : "text-gray-500",
-                      "h-5 w-5 flex-shrink-0",
-                    )}
-                    aria-hidden="true"
-                  />
-                ))}
-              </div>
-              <p className="mt-2 text-xs text-gray-500">
-                {review.rating.toFixed(1)} out of 5 stars
-              </p>
-            </div>
-          </div>
-
-          <div className="prose-sm prose mt-4 max-w-none text-gray-500">
-            {review.has_spoilers ? (
-              <HideableReviewBody body={review.body} />
-            ) : (
-              <p className=" text-gray-500">{review.body}</p>
-            )}
-          </div>
-
-          <div className="mt-8 flex justify-between">
-            <ReviewLikeButtonOptimistic
-              id={review.id}
-              vote_count={review.vote_count}
-              // is_liked={review.is_liked}
-            />
-            <ReviewDeleteButton id={review.id} />
-          </div>
-=======
-export const Review = ({ review }: Props) => {
+export const Review2 = ({ review }: Props) => {
   const session = useSession();
 
   const isAuthUsers = Number(session.data?.user.id) === review.author.id;
 
   return (
     <article className="border border-blue-500 p-4">
-      <p>{review.rating}/5 ⭐</p>
-      <p>{genRelativeDateStr(review.created_at)}</p>
+      <p className=" text-gray-500">{review.rating}/5 ⭐</p>
+      <p className="text-gray-600">{genRelativeDateStr(review.created_at)}</p>
       <div>
         <div className="flex">
           <Image
@@ -117,24 +45,90 @@ export const Review = ({ review }: Props) => {
               {review.author.username}
             </Link>{" "}
           </h4>
->>>>>>> Stashed changes
         </div>
       </div>
-<<<<<<< Updated upstream
-    </>
-=======
       {review.has_spoilers ? (
         <HideableReviewBody body={review.body ?? ""} />
       ) : (
-        <p>{review.body}</p>
+        <p className=" text-gray-500">{review.body}</p>
       )}
-      <ReviewLikeButtonOptimistic
+      <ReviewLikeButton
         id={review.id}
         vote_count={review.vote_count}
+
         // is_liked={review.is_liked}
       />
       {isAuthUsers && <ReviewDeleteButton id={review.id} />}
     </article>
->>>>>>> Stashed changes
+  );
+};
+
+export const Review = ({ review, index }: Props) => {
+  const session = useSession();
+
+  const isAuthUsers = Number(session.data?.user.id) === review.author.id;
+
+  return (
+    <article
+      key={review.id}
+      className="flex w-full space-x-4 text-sm text-gray-500"
+    >
+      <div
+        className={createClassName(
+          index === 0 ? "py-4" : "border-t border-gray-400 py-10",
+          "w-full",
+        )}
+      >
+        <div className="flex justify-between ">
+          <div>
+            <Link href={`/profiles/${review.author.username}`}>
+              <h3 className="font-medium text-gray-400">
+                {review.author.username}
+              </h3>
+            </Link>
+            <p className="text-gray-600">
+              {genRelativeDateStr(review.created_at)}
+            </p>
+          </div>
+
+          <div>
+            <div className="flex items-center">
+              {[0, 1, 2, 3, 4].map((rating) => (
+                <StarIcon
+                  key={rating}
+                  className={createClassName(
+                    review.rating > rating
+                      ? "text-yellow-400"
+                      : "text-gray-500",
+                    "h-5 w-5 flex-shrink-0",
+                  )}
+                  aria-hidden="true"
+                />
+              ))}
+            </div>
+            <p className="mt-2 text-xs text-gray-500">
+              {review.rating.toFixed(1)} out of 5 stars
+            </p>
+          </div>
+        </div>
+
+        <div className="prose-sm prose mt-4 max-w-none text-gray-500">
+          {review.has_spoilers ? (
+            <HideableReviewBody body={review.body} />
+          ) : (
+            <p className=" text-gray-500">{review.body}</p>
+          )}
+        </div>
+
+        <div className="mt-8 flex justify-between">
+          <ReviewLikeButton
+            id={review.id}
+            vote_count={review.vote_count}
+            // is_liked={review.is_liked}
+          />
+          {isAuthUsers && <ReviewDeleteButton id={review.id} />}
+        </div>
+      </div>
+    </article>
   );
 };
--- src/utils/string-formatters.ts
+++ src/utils/string-formatters.ts
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
-export function createClassName(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-=======
 export const createClassName = (...classes: string[]) => {
   return classes.filter(Boolean).join(" ");
 };
->>>>>>> Stashed changes
