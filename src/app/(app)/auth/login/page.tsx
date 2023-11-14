7af74b4902f8d4a458bfbf58c33ec92198cba2ba
Hayley <hayley.mcc@gmail.com>
2023-11-14 12:18:14 +0000

    Moved auth files to app path. Altered styling to take into account side nav

--- src/app/(app)/auth/login/page.tsx
+++ src/app/(app)/auth/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "./LoginForm";
 
 export default function Login() {
   return (
-    <section className="flex h-screen w-screen flex-col items-center justify-center gap-2 p-5">
+    <section className="flex h-screen w-[50vw] flex-col items-center gap-2 p-10">
       <h1 className="text-center text-xl font-bold">Login to your Account</h1>
       <LoginForm />
       <p className="text-center font-semibold">
--- src/app/(app)/auth/register/page.tsx
+++ src/app/(app)/auth/register/page.tsx
@@ -6,14 +6,12 @@ import { redirect } from "next/navigation";
 export default async function Register() {
   const session = await getServerAuthSession();
 
-  console.log(session);
-
   // If user already logged in
   if (session && session.user) {
     return redirect("/");
   }
   return (
-    <section className="flex flex-col items-center gap-2 p-5">
+    <section className=" flex h-screen w-[50vw] flex-col items-center gap-2 p-10">
       <h1 className="text-center text-xl font-bold">
         Register for a new account
       </h1>
