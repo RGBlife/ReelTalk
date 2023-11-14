385ec48fc58e8a622c8b4db3d396836a9df8d2c9
Bret <bret_gomes811@hotmail.co.uk>
2023-11-14 21:57:50 +0000

    add search case-sensitive and error handling

--- src/app/(app)/movies/MoviesScreen.tsx
+++ src/app/(app)/movies/MoviesScreen.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { fetchMovies } from "~/app/utils/api/fetchMovies";
 import { fetchMoviesAction } from "./component/fetchMoviesAction";
+import { set } from "date-fns";
 
 type Props = {
   movies: Movie[];
@@ -22,19 +23,37 @@ export function MoviesScreen({
   const [currentPage, setCurrentPage] = useState(1);
   const [endOfPage, setEndOfPage] = useState(false);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchError, setSearchError] = useState<string>("");
 
   const handlePageChange = async (newPage: number) => {
-    const result = await fetchMovies({ limit: 8, page: newPage });
-    if (!result || result.length === 0) return setEndOfPage(true);
-    setEndOfPage(false);
-    setMovies(result);
-    setCurrentPage(newPage);
+    try {
+      const result = await fetchMovies({ limit: 8, page: newPage });
+      if (!result || result.length === 0) return setEndOfPage(true);
+      setEndOfPage(false);
+      setMovies(result);
+      setCurrentPage(newPage);
+    } catch (error) {
+      setSearchError("An error occurred while fetching movies.");
+    }
   };
 
   useEffect(() => {
     const handleSearchTerm = async (searchTerm: string) => {
-      const result = await fetchMoviesAction(searchTerm);
-      setMovies(result);
+      setIsSearching(true);
+      setSearchError("");
+
+      try {
+        const result = await fetchMoviesAction(searchTerm);
+        if (result.length === 0) {
+          return setSearchError("No movies found, please try different keywords.");
+        } else {
+          setMovies(result);
+        }
+      } catch (error) {
+        setSearchError("An error occurred while fetching movies.");
+      }
+      setIsSearching(false);
     };
 
     if (searchTerm) {
@@ -59,33 +78,37 @@ export function MoviesScreen({
         }}
       />
 
-      <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
-        {movies.map((movie) => (
-          <li
-            key={movie.id}
-            className="max-w-xs overflow-hidden rounded bg-primary shadow-lg"
-          >
-            <Link href={`/movies/${movie.id}`}>
-              <Image
-                src={movie.poster_url}
-                alt={movie.title}
-                width={164}
-                height={246}
-                sizes="100vw"
-                style={{
-                  width: "100%",
-                  height: "auto",
-                }}
-              />
-            </Link>
-            <div className="px-6 py-4">
-              <p className="mb-2 text-xl font-bold text-neutral">
-                {movie.title}
-              </p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {searchError ? (
+        <p className="text-slate-950">{searchError}</p>
+      ) : (
+        <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+          {movies.map((movie) => (
+            <li
+              key={movie.id}
+              className="bg-primary max-w-xs overflow-hidden rounded shadow-lg"
+            >
+              <Link href={`/movies/${movie.id}`}>
+                <Image
+                  src={movie.poster_url}
+                  alt={movie.title}
+                  width={164}
+                  height={246}
+                  sizes="100vw"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                  }}
+                />
+              </Link>
+              <div className="px-6 py-4">
+                <p className="text-neutral mb-2 text-xl font-bold">
+                  {movie.title}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
 
       <div className="mt-8 flex justify-center">
         <button
