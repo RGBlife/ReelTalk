83d8a43cf98eedc42c1133df2a5a0869db8981ce
unknown <xandert.93@outlook.com>
2023-11-14 12:45:03 +0000

    adds initial MoviePreview to profile page

--- src/app/profiles/[username]/components/UserMovieList.tsx
+++ src/app/profiles/[username]/components/UserMovieList.tsx
@@ -1,5 +1,7 @@
+import { Movie } from "@prisma/client";
 import Link from "next/link";
 import { db } from "~/server/db";
+import { convertMinsToHoursStr } from "~/utils/date-formatters";
 
 export const getMoviesToWatch = async (userId: number) => {
   const mtws = await db.moviesToWatch.findMany({
@@ -23,12 +25,51 @@ export const UserMovieList = async ({ userId }: Props) => {
     <div>
       <div className="mx-auto max-w-2xl p-4">
         <div className="rounded-md bg-white p-4 shadow-md">
-          <h2>My Movies</h2>
+          <h2>My Watch Later Movies</h2>
           {movies.map((movie) => (
             <Link key={movie.id} href={`/movies/${movie.id}`}>
-              {movie.title}
+              <MoviePreview {...movie} />
             </Link>
           ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const MoviePreview = (movie: Movie) => {
+  const {
+    title,
+    overview,
+    release_date,
+    imdb_rating,
+    vote_count,
+    poster_url,
+    runtime,
+    trailer_url,
+  } = movie;
+
+  return (
+    <div className="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-lg">
+      <img
+        className="h-48 w-full object-cover object-center"
+        src={poster_url}
+        alt={title}
+      />
+
+      <div className="p-6">
+        <h2 className="text-xl font-bold text-gray-800">
+          {title} ({release_date.getFullYear()})
+        </h2>
+        <div className="mt-4 flex items-center justify-between">
+          <p className="text-gray-700">
+            Runtime: {convertMinsToHoursStr(runtime)}
+          </p>
+        </div>
+
+        <div className="mt-4 flex items-center justify-between">
+          <p className="text-gray-700">{imdb_rating.toFixed(1)} ⭐</p>
+          <p className="text-gray-700">Vote Count: {vote_count}</p>
         </div>
       </div>
     </div>
--- src/utils/date-formatters.ts
+++ src/utils/date-formatters.ts
@@ -1,14 +1,18 @@
 import { formatDistanceToNowStrict } from "date-fns";
 
-export const getYearFromDateStr = (dateStr: string) => {
-  const date = new Date(dateStr);
-
-  return date.getFullYear();
-};
-
 export const genRelativeDateStr = (date: Date) => {
   const relativeDateStr = formatDistanceToNowStrict(date, { addSuffix: true });
 
   if (relativeDateStr === "0 seconds ago") return "Just now";
   else return relativeDateStr;
+};
+
+export const convertMinsToHoursStr = (mins: number) => {
+  const hours = Math.floor(mins / 60);
+  const remainingMins = mins % 60;
+
+  const hoursString = hours > 0 ? `${hours}h ` : "";
+  const minutesString = remainingMins > 0 ? `${remainingMins}m` : "";
+
+  return hoursString + minutesString;
 };
