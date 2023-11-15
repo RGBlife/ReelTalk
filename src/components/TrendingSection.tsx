bda2f7f598ac35e3ee95a418567752c688ddc982
Brums16 <chrisbrummitt16@hotmail.co.uk>
2023-11-15 10:51:20 +0000

    reverted trending section changes

--- src/components/TrendingSection.tsx
+++ src/components/TrendingSection.tsx
@@ -15,21 +15,23 @@ export default async function TrendingSection() {
 
   return (
     <section>
-      <div className="flex flex-col content-center w-1/2 m-5 rounded border-[#b895f7] bg-gray-900 text-white p-10 border-4">
-      <h3 className="text-2xl text-center">Today's Trending Movies</h3>
-      </div>
-      <div className="flex flex-row gap-10 p-5 w-1/2">
+      <h3>Trending Now </h3>
+      <h3>Trending Chatroom </h3>
+      <div className="flex flex-row gap-5">
         {trendingMovies.map((movie) => {
           return <>
-          <div className="w-64 h-96 relative border-transparent rounded border-4 hover:border-[#b895f7]" >
-            <Link href={`/movies/${movie.id}`}>
+          <Link href={`/movies/${movie.id}`}>
               <Image
                 src={movie.poster_url}
-                fill={true}
+                width={20}
+                height={60}
                 alt={movie.title}
-                />
+                style={{
+                  maxWidth: "100%",
+                  height: "auto"
+                }} />
             </Link>
-            </div>
+
           </>;
         })}
         </div>
