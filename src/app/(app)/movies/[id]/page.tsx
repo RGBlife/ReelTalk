4479e5b6f1456e21ad28b8860bb41a727f048b51
unknown <xandert.93@outlook.com>
2023-11-15 16:49:14 +0000

    adds initial seen movie functionality

--- src/app/(app)/movies/[id]/page.tsx
+++ src/app/(app)/movies/[id]/page.tsx
@@ -1,12 +1,37 @@
+import { getAuthUser } from "~/server/auth";
 import { MovieDetailSection } from "./components/MovieDetailSection";
 import { ReviewSection } from "./components/ReviewSection";
 import { db } from "~/server/db";
 
-const getMovieById = (id: number) => {
-  return db.movie.findUniqueOrThrow({
+const getMovieById = async (id: number, authUserId: number) => {
+  const listFilter = {
+    where: {
+      movie_id: id,
+      user_id: authUserId,
+    },
+  };
+
+  const movie = await db.movie.findUniqueOrThrow({
     where: { id },
-    include: { genres: true },
+    include: {
+      genres: true,
+      ...(authUserId && {
+        moviesToWatch: listFilter,
+        moviesSeen: listFilter,
+      }),
+    },
   });
+
+  const updatedMovie = {
+    ...movie,
+    isWatchLater: Boolean(movie.moviesToWatch?.length),
+    isSeen: Boolean(movie.moviesSeen?.length),
+  };
+
+  // delete updatedMovie.moviesToWatch // 🚧
+  // delete updatedMovie.moviesSeen // 🚧
+
+  return updatedMovie;
 };
 
 type Context = {
@@ -16,8 +41,10 @@ type Context = {
 };
 
 export default async function SingleMoviePage({ params }: Context) {
+  const authUser = await getAuthUser();
+
   const movieId = Number(params.id);
-  const movie = await getMovieById(movieId);
+  const movie = await getMovieById(movieId, Number(authUser?.id));
 
   return (
     <>
