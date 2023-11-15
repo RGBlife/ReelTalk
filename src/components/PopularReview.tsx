881d6673a074aec03a5d99cd2cee7f52e4b1dc62
Brums16 <chrisbrummitt16@hotmail.co.uk>
2023-11-15 10:55:03 +0000

    reverted popular review

--- src/components/PopularReview.tsx
+++ src/components/PopularReview.tsx
@@ -22,35 +22,28 @@ export default async function PopularReview() {
 
   return (
     <section>
-      <div className="flex flex-col content-center w-1/2 m-5 rounded border-[#b895f7] bg-gray-900 text-white p-10 border-4">
-      <h3 className="text-2xl text-center">Most Popular Reviews</h3>
-      </div>
-      <div className="flex flex-row gap-10 p-5 w-1/2">
-        
+      <h3>Most Popular Reviews</h3>
+      <div className="flex flex-row gap-5">
         {reviews.map((review) => {
           return (
             <>
-            <div className="flex flex-col gap-5 border-transparent rounded border-4 p-5 hover:border-[#b895f7] hover:cursor-pointer ">
-            <h2 className="text-xl"> “{review.title}”</h2>
-            <div className="w-48 h-72 relative" >
               <Link href={`/movies/${review.movie_id}`}>
                 {movies.map((movie) => {
                   if (movie.id === review.movie_id) {
                     return (
                       <>
+                        <h2>{movie.title}</h2>
                         <img
                           src={movie.poster_url}
                           alt={movie.title}
+                          className="w-[10vw]"
                         />
                       </>
                     );
                   }
                 })}
-                </Link>
-                <h2>by {review.author_id}</h2>
-                </div>
-                </div>              
-              
+                <h2>{review.title}</h2>
+              </Link>
             </>
           );
         })}
