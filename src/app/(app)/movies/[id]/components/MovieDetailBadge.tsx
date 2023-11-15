5987abd94246759c28606ba9d6da5323c5cadd82
Bret <bret_gomes811@hotmail.co.uk>
2023-11-15 18:56:05 +0000

    minor style updates

--- src/app/(app)/movies/[id]/components/MovieDetailBadge.tsx
+++ src/app/(app)/movies/[id]/components/MovieDetailBadge.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export function MovieDetailBadge({ text }: Props) {
   return (
-    <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-gray-600">
+    <span className="inline-flex items-center rounded-md bg-gray-200 px-2 py-1 text-xs font-medium text-gray-600">
       {text}
     </span>
   );
