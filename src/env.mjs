7aefbb4ab81dfc37c9305cc81c926c54e5ccede4
Bret <bret_gomes811@hotmail.co.uk>
2024-02-07 19:27:05 +0000

    fix validation

--- src/env.mjs
+++ src/env.mjs
@@ -9,7 +9,6 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z
       .string()
-      .url()
       .refine(
         (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
         "You forgot to change the default URL",
